0ff9aba70ece12c52b71bf9ff00b40a5f8fcdcef
P-koushik <pkoushik224@gmail.com>
2026-02-07 16:38:16 +0530

    fix: format JSX in MessagesList component for better readability

--- app/components/message-list.tsx
+++ app/components/message-list.tsx
@@ -2,9 +2,9 @@ import SearchBar from "./search-bar";
 
 export default function MessagesList() {
   return (
-  <div className="p-2">
-    <SearchBar/>
-    Chat List Component
-    
-    </div>)
+    <div className="p-2">
+      <SearchBar />
+      Chat List Component
+    </div>
+  );
 }
